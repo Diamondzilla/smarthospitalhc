ca2ec9e7bca2ff29315bb2cdd2924c968c331111
Diamondzilla <haziqabhinaya@gmail.com>
2026-06-25 16:25:05 +0700

    Fix HTML formatting in symptoms section

--- hospital_app.py
+++ hospital_app.py
@@ -81,9 +81,9 @@ with st.form("triage_form"):
   st.markdown("""
 <div style="background-color: grey;border: 1px solid blue;border-radius: 10%;padding: 20px 24px; margin-bottom:20px;">
 <div style='display:flex;align-items:center;gap:10px;margin-bottom:4px;">
-<span style="background-color:wheat;color:black;border-radius:10%;"
-            what are your main symptoms></span>
-            <span style"color:white".Select all that apply</span>
+<span style="background-color:wheat;color:black;border-radius:10%;">
+            what are your main symptoms</span>
+            <span style"color:white">Select all that apply</span>
             </div>
             </div>
             """, unsafe_allow_html=True)
